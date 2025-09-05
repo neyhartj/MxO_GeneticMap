e4b8d669b099f99f85e0f17d0acd8195e34ae359
neyhartj <neyhartje@gmail.com>
2025-09-04 22:05:27 -0400

    further edits to alignment script.

--- code/05_GenomeAlignment/05_run_alignment_and_syri_stevens_oxycoccos.sh
+++ code/05_GenomeAlignment/05_run_alignment_and_syri_stevens_oxycoccos.sh
@@ -58,21 +58,41 @@ mkdir -p $RESULTS/$SUBDIR
 # Define outdir
 OUTDIR=$RESULTS/$SUBDIR
 
+
+# Modify the REF and QUERY fasta files to make sure the chromosome names are identical
+# This is important for SYRI to work properly
+# The REF fasta chromosome name follow the pattern "chr[#]_Vaccinium_macrocarpon_Stevens_v1". We will rename them to "chr[#]"
+# The QUERY fasta chromosome names follow the pattern "chr[#]_Vaccinium_macrocarpon_Stevens_v1_RagTag". We will rename them to "chr[#]"
+# For both genomes, we will only keep the chromosomes that start with "chr" (i.e. we will remove any contigs that do not belong to a chromosome)
+# Create modified versions of the fasta files in the outdir
+NEWGENOMEDIR=$RESULTS/modified_genomes
+mkdir -p $NEWGENOMEDIR
+
+# Name of the modified reference fasta
+newstevens=${NEWGENOMEDIR}/V_macrocarpon_Stevens_v1_renamed.fasta
+# Rename the chromosomes in the reference fasta and only keep those that start with "chr"
+awk '/^>/{sub(/_Vaccinium_macrocarpon_Stevens_v1/,"");}1' $REF | awk '/^>chr/{print; getline; print}' > $newstevens
+
+# Name of the modified query fasta
+oxyassembly=${NEWGENOMEDIR}/Voxycoccos_NJ96-20_v1_ragtag_scaffolded_renamed.fasta
+# Rename the chromosomes in the query fasta
+awk '/^>/{sub(/_Vaccinium_macrocarpon_Stevens_v1_RagTag/,"");}1' $QUERY | awk '/^>chr/{print; getline; print}' > $oxyassembly
+
 # Define a subdir for nucmer results
 NUCMER_OUT=$OUTDIR/nucmer
 mkdir -p $NUCMER_OUT
 NUCMER_PREFIX=${NUCMER_OUT}/stevens_oxy_nucmer
-# Align the oxycoccos genome to the stevens using stevens as the reference
-nucmer --maxmatch --noextend -c 500 -b 500 -l 100 -p $NUCMER_PREFIX $REF $QUERY
+# Align the oxycoccos genome to the stevens using stevens as the reference; use the renamed versions of the fasta files
+nucmer --maxmatch --noextend -c 500 -b 500 -l 100 -p $NUCMER_PREFIX $newstevens $oxyassembly
 
 # Filter for 1-1 alignments with at least 90% identity and at least 100 bp long
 delta-filter -m -i 90 -l 100 $NUCMER_PREFIX.delta > ${NUCMER_PREFIX}_i90_l100.delta
 
-# Produce a dotplot
-MUMMERPLOT_OUT=$OUTDIR/mummerplot
-mkdir -p $MUMMERPLOT_OUT
-MUMMERPLOT_PREFIX=${MUMMERPLOT_OUT}/stevens_oxy_mummerplot
-mummerplot --png --large --layout -p ${MUMMERPLOT_PREFIX} ${NUCMER_PREFIX}_i90_l100.delta
+# # Produce a dotplot
+# MUMMERPLOT_OUT=$OUTDIR/mummerplot
+# mkdir -p $MUMMERPLOT_OUT
+# MUMMERPLOT_PREFIX=${MUMMERPLOT_OUT}/stevens_oxy_mummerplot
+# mummerplot --png --large --layout -p ${MUMMERPLOT_PREFIX} ${NUCMER_PREFIX}_i90_l100.delta
 
 # Convert to tab of coordinates
 show-coords -THrd ${NUCMER_PREFIX}_i90_l100.delta > ${NUCMER_PREFIX}_i90_l100.coords
@@ -83,7 +103,8 @@ SYRI_OUT=$OUTDIR/syri
 mkdir -p $SYRI_OUT
 SYRI_PREFIX=stevens_oxy_syri
 
-syri -r $REF -q $QUERY \
+# Run SYRI using the filtered nucmer alignments and the renamed fasta files
+syri -r $newstevens -q $oxyassembly \
 	-c ${NUCMER_PREFIX}_i90_l100.coords -d ${NUCMER_PREFIX}_i90_l100.delta \
 	--lf ${SYRI_PREFIX}.log --log DEBUG \
 	-k \
@@ -93,9 +114,9 @@ syri -r $REF -q $QUERY \
 # 
 
 
-# # Visualize
-# echo -e "#file\tname\ttags\n${newstevens2}\tstevens\n${oxyassembly}\toxycoccos" > ${OUTDIR}_genomes.txt
-# plotsr --sr ${SYRI_PREFIX}.out --genomes ${OUTDIR}_genomes.txt -o ${SYRI_PREFIX}_plot.png
+# Visualize
+echo -e "#file\tname\ttags\n${newstevens2}\tstevens\n${oxyassembly}\toxycoccos" > ${SYRI_OUT}_genomes.txt
+plotsr --sr ${SYRI_PREFIX}.out --genomes ${SYRI_OUT}_genomes.txt -o ${SYRI_PREFIX}_plot.png
 
-
+# End of script
 
